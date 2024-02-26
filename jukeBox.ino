4330b6d8903119569ac4d0945add1fe7ea7c6c10
smart-life-tech <christlightworld@gmail.com>
2024-02-26 21:24:46 +0100

    modified:   jukeBox.ino

--- jukeBox.ino
+++ jukeBox.ino
@@ -81,8 +81,8 @@ void loop()
             Serial.println(keyBuffer);
             // ***************
             int intKey = atoi(keyBuffer);
-            //myDFPlayer.play(intKey);
-            // ####################################################
+            // myDFPlayer.play(intKey);
+            //  ####################################################
             lcd.setCursor(16, currentDisplayLine);
             lcd.print(" ");                        // overwrite old data
             lcd.setCursor(16, currentDisplayLine); // reset cursor
@@ -197,8 +197,13 @@ void loop()
 }
 void playTheList()
 {
+
     static unsigned long timer = 0;
     unsigned long interval = 100;
+    if (digitalRead(busyPin) == 0) // has it gone from low to high?, meaning the track finished
+    {
+        Serial.print("music still playing ");
+    }
     if (millis() - timer >= interval)
     {
         timer = millis();
