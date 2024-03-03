995a9c594a496b515d06933889774c2cba71ff32
smart-life-tech <christlightworld@gmail.com>
2024-03-03 10:57:16 +0100

    modified:   jukeBox.ino

--- jukeBox.ino
+++ jukeBox.ino
@@ -10,11 +10,12 @@ byte currentSelection = 1; // Tracks the current selection (1st, 2nd, 3rd)
 const byte ROWS = 4;       // four rows
 const byte COLS = 4;       // three columns
 char keys[ROWS][COLS] =
-    {
-        {'1', '2', '3', 'A'},
-        {'4', '5', '6', 'B'},
-        {'7', '8', '9', 'C'},
-        {'*', '0', '#', 'D'}};
+{
+  {'1', '2', '3', 'A'},
+  {'4', '5', '6', 'B'},
+  {'7', '8', '9', 'C'},
+  {'*', '0', '#', 'D'}
+};
 const byte busyPin = 10;
 const byte buttonPin = 8;
 const byte pinLed = 5;
@@ -46,71 +47,79 @@ int sequenceLength = 0;                // Current length of the sequence list
 // Function to add a track to the sequence list
 void addToSequenceList(int trackNumber)
 {
-    if (sequenceLength < MAX_SEQUENCE_LENGTH)
-    {
-        sequenceList[sequenceLength] = trackNumber;
-        sequenceLength++;
-        Serial.print("Track ");
-        Serial.print(trackNumber);
-        Serial.println(" added to sequence list");
-    }
-    else
-    {
-        Serial.println("Sequence list is full");
-    }
+  if (sequenceLength < MAX_SEQUENCE_LENGTH)
+  {
+    sequenceList[sequenceLength] = trackNumber;
+    sequenceLength++;
+    Serial.print("Track ");
+    Serial.print(trackNumber);
+    Serial.println(" added to sequence list");
+  }
+  else
+  {
+    Serial.println("Sequence list is full");
+  }
 }
 
 // Function to play the sequence
 void playSequence()
 {
-    for (int i = 0; i < sequenceLength; i++)
-    {
-        // myDFPlayer.playFolder(1, sequenceList[i]); // Assuming track numbers are in folder 1
-        // delay(1000);                               // Adjust delay as needed
-    }
+  for (int i = 0; i < sequenceLength; i++)
+  {
+    // myDFPlayer.playFolder(1, sequenceList[i]); // Assuming track numbers are in folder 1
+    // delay(1000);                               // Adjust delay as needed
+  }
 }
 
 // Function to stop the sequence
 void stopSequence()
 {
-    playList = false;
-    playIndex = 0;
+  playList = false;
+  playIndex = 0;
 
-    myDFPlayer.stop();
+  myDFPlayer.stop();
 }
 
 void setup()
 {
-    Serial.begin(115200);
-    Serial.print(F("Enter track number then enter action"));
-    Serial.println(F(" # = ENTER"));
-    Serial.println(F(" * = Play immediate"));
-    Serial.println(F(" A = Add to sequence list"));
-    Serial.println(F(" B = Play sequence"));
-    Serial.println(F("To add track 18 to the list, \"18A\""));
-    Serial.println(F("To play track 3 immediately, \"3*\""));
-    Serial.println(F(" C = STOP sequence"));
-    Serial.println(F("\n\n"));
-    mp3ss.begin(9600);
-    myDFPlayer.begin(mp3ss);
-    myDFPlayer.volume(20);
-    // myDFPlayer.play(3);
-    lcd.begin(20, 4);
-    lcd.clear(); // the only time that you should use clear
-    lcd.print(" Halloween Sounds ");
-    lcd.setCursor(0, 1);
-    lcd.print(" 1st Selection <___> ");
-    lcd.setCursor(0, 2);
-    lcd.print(" 2nd Selection <___>");
-    lcd.setCursor(0, 3);
-    lcd.print(" 3rd Selection <___>");
+  Serial.begin(115200);
+  Serial.print(F("Enter track number then enter action"));
+  Serial.println(F(" # = ENTER"));
+  Serial.println(F(" * = Play immediate"));
+  Serial.println(F(" A = Add to sequence list"));
+  Serial.println(F(" B = Play sequence"));
+  Serial.println(F("To add track 18 to the list, \"18A\""));
+  Serial.println(F("To play track 3 immediately, \"3*\""));
+  Serial.println(F(" C = STOP sequence"));
+  Serial.println(F("\n\n"));
+  mp3ss.begin(9600);
+  myDFPlayer.begin(mp3ss);
+  myDFPlayer.volume(20);
+  // myDFPlayer.play(3);
+  lcd.begin(20, 4);
+  lcd.clear(); // the only time that you should use clear
+  lcd.print(" Halloween Sounds ");
+  lcd.setCursor(0, 1);
+  lcd.print(" 1st Selection <___> ");
+  lcd.setCursor(0, 2);
+  lcd.print(" 2nd Selection <___>");
+  lcd.setCursor(0, 3);
+  lcd.print(" 3rd Selection <___>");
 }
 void loop()
 {
-    if (playList)
-    {
-        playTheList();
-    }
+  if (playList)
+  {
+    playTheList();
+  }
+  key = keypad.getKey();
+  if (key)
+  {
+     Serial.print(F(" key code = "));
+     Serial.print(key);
+    getEntry(key);
+  }
+  /*
     if (mode == 0) // check for new key
     {
         key = keypad.getKey();
@@ -237,136 +246,137 @@ void loop()
         }
     }
     newEntry = false;
+  */
 }
 void playTheList()
 {
 
-    static unsigned long timer = 0;
-    unsigned long interval = 100;
-    if (digitalRead(busyPin) == 0) // has it gone from low to high?, meaning the track finished
-    {
-        // Serial.print("music still playing ");
-    }
-    if (millis() - timer >= interval)
-    {
-        timer = millis();
+  static unsigned long timer = 0;
+  unsigned long interval = 100;
+  if (digitalRead(busyPin) == 0) // has it gone from low to high?, meaning the track finished
+  {
+    // Serial.print("music still playing ");
+  }
+  if (millis() - timer >= interval)
+  {
+    timer = millis();
 
-        // %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-        bool busyPinState = digitalRead(busyPin);                           // read the busy pin
-        if (busyPinState != lastBusyPinState && playIndex < sequenceLength) // has it changed?
+    // %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    bool busyPinState = digitalRead(busyPin);                           // read the busy pin
+    if (busyPinState != lastBusyPinState && playIndex < sequenceLength) // has it changed?
+    {
+      if (busyPinState == 1) // has it gone from low to high?, meaning the track finished
+      {
+        Serial.print("play index = ");
+        Serial.println(sequenceLength);
+        myDFPlayer.play(sequenceList[sequenceLength]);
+        playIndex++;        // next track
+        if (playIndex >= 3) // last track?
         {
-            if (busyPinState == 1) // has it gone from low to high?, meaning the track finished
-            {
-                Serial.print("play index = ");
-                Serial.println(sequenceLength);
-                myDFPlayer.play(sequenceList[sequenceLength]);
-                playIndex++;        // next track
-                if (playIndex >= 3) // last track?
-                {
-                    playIndex = 0;      // reset list
-                    keyBuffer[0] = 'C'; // set up for stop mode
-                    mode = 6;           // call stop mode
-                    playList = false;
-                }
-            }
-            lastBusyPinState = busyPinState; // remember the last busy state
-
-            // %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+          playIndex = 0;      // reset list
+          keyBuffer[0] = 'C'; // set up for stop mode
+          mode = 6;           // call stop mode
+          playList = false;
         }
+      }
+      lastBusyPinState = busyPinState; // remember the last busy state
+
+      // %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
     }
+  }
 }
 
 void getEntry(char key)
 {
-    static boolean entryStarted = false;
-    static byte keyBufferIndex = 0;
-    // Increment current selection or wrap back to 1
-    if (key == 'A' || key == 'C')
+  static boolean entryStarted = false;
+  static byte keyBufferIndex = 0;
+  // Increment current selection or wrap back to 1
+  if (key == 'A' || key == 'C')
+  {
+    currentSelection++;
+    row = 15;
+    if (currentSelection > 3)
     {
-        currentSelection++;
-        row = 15;
-        if (currentSelection > 3)
-        {
-            currentSelection = 1;
-        }
+      currentSelection = 1;
     }
-    if (key == 'D')
-    { // Delete last key entry
-        if (keyBufferIndex > 0)
-        {
-            memset(keyBuffer, 0, sizeof(keyBuffer));
-            keyBufferIndex = 0;
+  }
+  if (key == 'D')
+  { // Delete last key entry
+    if (keyBufferIndex > 0)
+    {
+      memset(keyBuffer, 0, sizeof(keyBuffer));
+      keyBufferIndex = 0;
 
-            // Serial.print(F("\t\t"));
-            Serial.print(keyBuffer[keyBufferIndex - 1]);
-            Serial.println(F(" deleted"));
-            keyBufferIndex--;
-            return;
-        }
+      // Serial.print(F("\t\t"));
+      Serial.print(keyBuffer[keyBufferIndex - 1]);
+      Serial.println(F(" deleted"));
+      keyBufferIndex--;
+      return;
     }
-    else if (key == '#')
-    { // End of entry
-        keyBuffer[keyBufferIndex] = '\0';
-        entryStarted = false;
-        newEntry = true;
+  }
+  else if (key == '#')
+  { // End of entry
+    keyBuffer[keyBufferIndex] = '\0';
+    entryStarted = false;
+    newEntry = true;
+  }
+  else if (key == '*' || key == 'A' || key == 'B' || key == 'C')
+  { // Action keys
+    if (keyBufferIndex > 0)
+    { // Check if a track number has been entered
+      switch (key)
+      {
+        case '*': // Play immediate
+          Serial.println(F(" play immediate"));
+          myDFPlayer.play( atoi(keyBuffer)); // Assuming track numbers are in folder 1
+          break;
+        case 'A': // Add to sequence list
+          Serial.println(F(" add to list"));
+          addToSequenceList(atoi(keyBuffer));
+          break;
+        case 'B': // Play sequence
+          Serial.println(F(" play the sequence"));
+          playList = true;
+          playSequence();
+          break;
+        case 'C': // STOP sequence
+          Serial.println(F(" stop the playing"));
+          stopSequence();
+          break;
+        default:
+          break;
+      }
     }
-    else if (key == '*' || key == 'A' || key == 'B' || key == 'C')
-    { // Action keys
-        if (keyBufferIndex > 0)
-        { // Check if a track number has been entered
-            switch (key)
-            {
-            case '*': // Play immediate
-                Serial.println(F(" play immediate"));
-                myDFPlayer.play(atoi(keyBuffer)); // Assuming track numbers are in folder 1
-                break;
-            case 'A': // Add to sequence list
-                Serial.println(F(" add to list"));
-                addToSequenceList(atoi(keyBuffer));
-                break;
-            case 'B': // Play sequence
-                Serial.println(F(" play the sequence"));
-                playList = true;
-                playSequence();
-                break;
-            case 'C': // STOP sequence
-                Serial.println(F(" stop the playing"));
-                stopSequence();
-                break;
-            default:
-                break;
-            }
-        }
-        // Clear the buffer
-        memset(keyBuffer, 0, sizeof(keyBuffer));
-        keyBufferIndex = 0;
+    // Clear the buffer
+    memset(keyBuffer, 0, sizeof(keyBuffer));
+    keyBufferIndex = 0;
+  }
+  else if (entryStarted == false && isDigit(key))
+  { // Start of new entry
+    keyBufferIndex = 0;
+    entryStarted = true;
+    keyBuffer[keyBufferIndex] = key;
+    newEntry = false;
+    keyBufferIndex++;
+  }
+  else if (entryStarted == true && key != '#' && key != '*')
+  { // Continue entry
+    if (keyBufferIndex < 4 && isDigit(key))
+    {
+      keyBuffer[keyBufferIndex] = key;
+      keyBufferIndex++;
+      // Update LCD screen with entered track number for current selection
+      lcd.setCursor(row, currentSelection); // Adjust the cursor position as needed
+      lcd.print(key);
+      row++;
     }
-    else if (entryStarted == false && isDigit(key))
-    { // Start of new entry
-        keyBufferIndex = 0;
-        entryStarted = true;
-        keyBuffer[keyBufferIndex] = key;
-        newEntry = false;
-        keyBufferIndex++;
-    }
-    else if (entryStarted == true && key != '#' && key != '*')
-    { // Continue entry
-        if (keyBufferIndex < 4 && isDigit(key))
-        {
-            keyBuffer[keyBufferIndex] = key;
-            keyBufferIndex++;
-            // Update LCD screen with entered track number for current selection
-            lcd.setCursor(row, currentSelection); // Adjust the cursor position as needed
-            lcd.print(key);
-            row++;
-        }
-    }
+  }
 }
 float entryToFloat(char *entry)
 {
-    return (atof(entry));
+  return (atof(entry));
 }
 int entryToInt(char *entry)
 {
-    return (atoi(entry));
+  return (atoi(entry));
 }
